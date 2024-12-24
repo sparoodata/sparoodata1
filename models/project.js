6bb6169cfa3a993644b46a0863d2cfdd63687267
Glitch (glitch-hello-node) <none>
2024-12-24 23:48:37 +0000

    🦁🖐 Checkpoint ./models/project.js:65144299/838 ./server.js:65144299/4406

--- models/project.js
+++ models/project.js
@@ -4,7 +4,8 @@ const ProjectSchema = new mongoose.Schema({
   name: String,
   description: String,
   organization: { type: mongoose.Schema.Types.ObjectId, ref: 'Organization' },
-  created_by: { type: String, required: true }, // <-- Add this
+  created_by: { type: String, required: true }, // Make sure you're storing the user who created it
+  instances: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Instance' }], // <-- Add this
 });
 
 module.exports = mongoose.models.Project || mongoose.model('Project', ProjectSchema);
